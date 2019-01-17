3843181efa85c5d82dea6cc54144bb834463df50
Roman A. Taycher <rtaycher1987@gmail.com>
2019-01-17 03:55:26 -0800

    somewhat working delete button (doesn't really work with networking)

--- clipboard.py
+++ clipboard.py
@@ -25,12 +25,17 @@ class ClipboardManager:
         self.hostname = socket.gethostname()
         self.clipboard_save_file = self.clipboard_save_dir / f"{self.hostname}.clips.json"
 
-    def read_clips_from_files(self):
+    def read_clips_from_files(self, initialize=False):
         print('running read_clips_from_files')
+        update_clips = []
         for json_filepath in self.clipboard_save_dir.glob("*.clips.json"):
+            if json_filepath == self.clipboard_save_file and not initialize:
+                continue
             with json_filepath.open() as f:
-                self.clips += json.load(f)
-        self.clips = list(set(self.clips))
+                update_clips += json.load(f)
+        for clip in update_clips:
+            if clip not in self.clips:
+                self.clips.append(clip)
         self.clips_model.setStringList(self.clips)
 
     def on_clip(self, mode):
@@ -45,6 +50,7 @@ class ClipboardManager:
             self.clipboard_save_dir = new_save_folder_path
             self.clipboard_save_file = self.clipboard_save_dir / f"{self.hostname}.clips.json"
             self.clipboard_save_dir_line_widget.setText(new_save_folder)
+
     def pick_save_folder(self):
         self.update_save_folder(QFileDialog.getExistingDirectory(self.window,
                                                                  "Pick shared network folder to sync clipboards",
@@ -55,6 +61,20 @@ class ClipboardManager:
         self.clips.insert(0, last_clipboard_contents)
         self.clips_model.setStringList(self.clips)
         print(f'new clipboard contents: {last_clipboard_contents}')
+        self.clipboard_save_dir.mkdir(parents=True, exist_ok=True)
+        with open(self.clipboard_save_file, 'w') as f:
+            json.dump(self.clips, f)
+
+    def copy_entry(self):
+        results = []
+        for index in self.clips_view.selectedIndexes():
+            results.append(self.clips[index.row()])
+        self.clipboard.setText('\n'.join(results))
+
+    def delete_entry(self):
+        for index in self.clips_view.selectedIndexes():
+            self.clips.pop(index.row())
+        self.clips_model.setStringList(self.clips)
         self.clipboard_save_dir.mkdir(parents=True, exist_ok=True)
         with open(self.clipboard_save_file, 'w') as f:
             json.dump(self.clips, f)
@@ -73,7 +93,7 @@ class ClipboardManager:
         self.clips_view = QListView()
         self.clips_model = QStringListModel()
         self.clips_view.setModel(self.clips_model)
-        self.read_clips_from_files()
+        self.read_clips_from_files(initialize=True)
         self.save_folders_label = QLabel("clipboard network saves folder")
         self.layout.addWidget(self.save_folders_label)
         line = QWidget()
@@ -81,12 +101,28 @@ class ClipboardManager:
         line.layout().addWidget(self.clipboard_save_dir_line_widget)
         line.layout().addWidget(self.launch_file_dialog_button)
         self.layout.addWidget(line)
+
+        self.copy_button = QPushButton()
+        self.copy_button.setText("Copy")
+        self.copy_button.clicked.connect(self.copy_entry)
+
+        self.delete_button = QPushButton()
+        self.delete_button.setText("Delete")
+        self.delete_button.clicked.connect(self.delete_entry)
+
+        buttons = QWidget()
+        buttons.setLayout(QHBoxLayout())
+        buttons.layout().addWidget(self.copy_button)
+        buttons.layout().addWidget(self.delete_button)
+
+        self.layout.addWidget(buttons)
+
         self.layout.addWidget(self.clips_view)
         self.window.setLayout(self.layout)
         self.clipboard.changed.connect(self.on_clip)
         self.timer = QTimer()
         self.timer.timeout.connect(self.read_clips_from_files)
-        self.timer.start(5*1000)
+        self.timer.start(5 * 1000)
         self.window.resize(QDesktopWidget().availableGeometry(self.window).size() * 0.5)
         self.window.show()
         sys.exit(app.exec_())
